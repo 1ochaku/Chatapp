23492ae4635718aae21cc02a7aada4ec674b54ea
Punyo Sunya <sunya22839@example.com>
2025-02-13 15:46:09 +0530

    initialised the frontend

---
+++ frontend/src/pages/Auth.tsx
@@ -0,0 +1,17 @@
+const Auth: React.FC = () => {
+    return (
+        <div>
+            {/* Header */}
+            <div>
+                <button>
+                    Login
+                </button>
+                <button>
+                    Signup
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Auth;
